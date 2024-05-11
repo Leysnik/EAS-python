6d2e8af8f2a37a22d4c96e24855cd532eed809ee
Трескин Никита Михайлович <treskin456@gmail.com>
2024-05-11 22:47:30 +0300

    update interface(js, build hist params)

---
+++ static/js/main_script.js
@@ -0,0 +1,48 @@
+let one_period_btn = document.getElementById("one_period");
+let list_period_btn = document.getElementById("list_periods");
+let categories_btn = document.getElementById("categories");
+
+let transfers = document.getElementById("transfers");
+let periods_tau = document.getElementById("periods_tau");
+let hlOperations = document.getElementById("HLoperations")
+
+let form = document.getElementById("build_form");
+
+one_period_btn.addEventListener("click", (event) => {
+    one_period_btn.classList.add("selected-type");
+    list_period_btn.classList.remove("selected-type");
+    categories_btn.classList.remove("selected-type");
+
+    periods_tau.classList.add("disabled");
+    transfers.classList.remove("disabled");
+    hlOperations.classList.remove("disabled")
+
+    form.action = "/build_one_period";
+});
+
+list_period_btn.addEventListener("click", (event) => {
+    one_period_btn.classList.remove("selected-type");
+    list_period_btn.classList.add("selected-type");
+    categories_btn.classList.remove("selected-type");
+
+    periods_tau.classList.remove("disabled");
+    transfers.classList.remove("disabled");
+    hlOperations.classList.remove("disabled")
+
+    form.action = "/build_list_period";
+});
+
+categories_btn.addEventListener("click", (event) => {
+    one_period_btn.classList.remove("selected-type");
+    list_period_btn.classList.remove("selected-type");
+    categories_btn.classList.add("selected-type");
+
+    periods_tau.classList.remove("disabled");
+    transfers.classList.remove("disabled");
+    hlOperations.classList.add("disabled")
+    form.action = "/build_categories";
+});
+
+window.addEventListener("load", () => {
+    list_period_btn.click();
+});
